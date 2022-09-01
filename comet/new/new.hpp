a0a648bbc9b5c6685692bd43edfa2ea704c848ca
plaristote <michael@unetresgrossebite.com>
2022-09-01 16:20:58 +0200

    makes build dir configurable from `comet new`

--- comet/new/new.hpp
+++ comet/new/new.hpp
@@ -10,6 +10,7 @@ class New : public Crails::Command
   std::string cheerp_path      = "/opt/cheerp";
   std::string html_output_path;
   std::string html_config_path;
+  std::string build_path;
 public:
   std::string_view description() const override { return "Create a new comet project"; }
   void options_description(boost::program_options::options_description&) const override;
--- comet/new/templates/build/CMakeLists.txt.cpp
+++ comet/new/templates/build/CMakeLists.txt.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include "crails/shared_vars.hpp"
 #include "crails/template.hpp"
+#include <vector>
 using namespace std;
 
 class ProjectCmakelistsTxt : public Crails::Template
@@ -11,7 +12,8 @@ public:
     project_name(Crails::cast<string>(vars, "project_name")), 
     output_name(Crails::cast<string>(vars, "output_name",  "application")), 
     rpath(Crails::cast<string>(vars, "rpath",  "/usr/local/lib/genericjs")), 
-    generated_files_dir(Crails::cast<string>(vars, "generated_files_dir",  "lib"))
+    generated_files_dir(Crails::cast<string>(vars, "generated_files_dir",  "lib")), 
+    external_sources(Crails::cast<vector<string>>(vars, "external_sources"))
   {}
 
   std::string render()
@@ -19,7 +21,12 @@ public:
 ecpp_stream << "# runs with the following option:\n# -DCMAKE_TOOLCHAIN_FILE \"$CHEERP_PATH/share/cmake/Modules/CheerpToolchain.cmake\"\n\ncmake_minimum_required(VERSION 3.0)\n\nproject(" << ( project_name );
   ecpp_stream << ")\ninclude_directories(/usr/local/include " << ( generated_files_dir );
   ecpp_stream << ")\nlink_directories(/usr/local/lib/genericjs /usr/lib/genericjs)\n\nset(CMAKE_CXX_FLAGS \"-target cheerp-genericjs -D__CHEERP_CLIENT__\")\nset(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -Wall -Wno-unknown-pragmas -pedantic\")\n\nif (CMAKE_BUILD_TYPE EQUAL \"DEBUG\")\n  set(CMAKE_EXE_LINKER_FLAGS \"${CMAKE_EXE_LINKER_FLAGS}\" -cheerp-sourcemap=" << ( output_name );
-  ecpp_stream << ".js.map -cheerp-sourcemap-standalone)\nendif()\n\nfile(GLOB_RECURSE app_src *.cpp *.cxx)\n\nadd_executable(" << ( output_name );
+  ecpp_stream << ".js.map -cheerp-sourcemap-standalone)\nendif()\n\nfile(GLOB_RECURSE app_src\n  *.cpp *.cxx";
+ for (const string& source : external_sources){
+  ecpp_stream << "\n  " << ( source );
+  ecpp_stream << "";
+ };
+  ecpp_stream << "\n)\n\nadd_executable(" << ( output_name );
   ecpp_stream << " ${app_src})\n\ntarget_link_libraries(" << ( output_name );
   ecpp_stream << "\n  crails-semantics\n  comet\n)\n";
     return ecpp_stream.str();
@@ -30,6 +37,7 @@ private:
   string output_name;
   string rpath;
   string generated_files_dir;
+  vector<string> external_sources;
 };
 
 std::string render_project_cmakelists_txt(const Crails::Renderer* renderer, Crails::SharedVars& vars)
