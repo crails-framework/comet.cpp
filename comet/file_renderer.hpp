636a1277f157cce54e00d9d110c6e1304d52e4d4
plaristote <michael@unetresgrossebite.com>
2022-09-02 12:17:53 +0200

    refactore FileRenderer

--- comet/file_renderer.hpp
+++ comet/file_renderer.hpp
@@ -1,17 +1,5 @@
 #pragma once
 #include "project_renderer.hpp"
-#include <crails/shared_vars.hpp>
-#include <boost/filesystem.hpp>
+#include <crails/cli/file_renderer.hpp>
 
-class FileRenderer
-{
-public:
-  ProjectRenderer    renderer;
-  Crails::SharedVars vars;
-  bool               should_overwrite = false;
-
-  bool generate_file(std::string_view template_name) { return generate_file(template_name, template_name); }
-  bool generate_file(std::string_view template_name, std::string_view filepath);
-private:
-  bool render_file(std::string_view template_name, boost::filesystem::path);
-};
+typedef Crails::FileRendererBase<ProjectRenderer> FileRenderer;
