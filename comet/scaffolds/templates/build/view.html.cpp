480d5deade3163fb2af1837a021846bc95857981
plaristote <michael@unetresgrossebite.com>
2022-09-04 18:21:48 +0200

    adds property option to view scaffold

--- comet/scaffolds/templates/build/view.html.cpp
+++ comet/scaffolds/templates/build/view.html.cpp
@@ -1,21 +1,35 @@
 #include <sstream>
 #include "crails/shared_vars.hpp"
 #include "crails/template.hpp"
+#include <map>
+using namespace std;
 
 class ScaffoldViewHtml : public Crails::Template
 {
 public:
   ScaffoldViewHtml(const Crails::Renderer* renderer, Crails::SharedVars& vars) :
-    Crails::Template(renderer, vars)
+    Crails::Template(renderer, vars), 
+    properties(reinterpret_cast<map<string,string>&>(*Crails::cast<map<string,string>*>(vars, "properties")))
   {}
 
   std::string render()
   {
-ecpp_stream << "<template>\n  <body>\n  </body>\n</template>\n";
+ecpp_stream << "<template>";
+ if (properties.size() > 0){
+  ecpp_stream << "\n  <head>";
+   for (auto it = properties.begin() ; it != properties.end() ; ++it){
+  ecpp_stream << "\n    <attribute name=\"" << ( it->first );
+  ecpp_stream << "\" type=\"" << ( it->second );
+  ecpp_stream << "\" />";
+ };
+  ecpp_stream << "\n  </head>";
+ };
+  ecpp_stream << "\n  <body>\n  </body>\n</template>\n";
     return ecpp_stream.str();
   }
 private:
   std::stringstream ecpp_stream;
+  map<string,string>& properties;
 };
 
 std::string render_scaffold_view_html(const Crails::Renderer* renderer, Crails::SharedVars& vars)
--- comet/scaffolds/view.hpp
+++ comet/scaffolds/view.hpp
@@ -14,13 +14,15 @@ public:
   void options_description(boost::program_options::options_description& desc) const override
   {
     desc.add_options()
-      ("name,n",   boost::program_options::value<std::string>(), "view name")
-      ("target,t", boost::program_options::value<std::string>(), "target folder");
+      ("name,n",     boost::program_options::value<std::string>(), "view name")
+      ("target,t",   boost::program_options::value<std::string>(), "target folder")
+      ("property,p", boost::program_options::value<std::vector<std::string>>()->multitoken(), properties_help());
   }
 
   int create(boost::program_options::variables_map& options) override
   {
     std::string comet_html_namespace = "HtmlTemplate";
+    std::map<std::string, std::string> properties = properties_option(options);
 
     if (options.count("name"))
       name = Crails::naming_convention.filenames(options["name"].as<std::string>());
@@ -31,6 +33,7 @@ public:
     renderer.vars["include"] = "templates/" + (target_folder.length() > 0 ? target_folder + '/' : "") + name + ".hpp";
     renderer.vars["element"] = comet_html_namespace + "::" + Crails::camelize(name);
     renderer.vars["classname"] = Crails::naming_convention.classnames(name);
+    renderer.vars["properties"] = &properties;
     renderer.generate_file("scaffolds/view.html", "views/" + target_folder + '/' + name + ".html");
     renderer.generate_file("scaffolds/view.hpp",  "views/" + target_folder + '/' + name + ".hpp");
     return 0;
